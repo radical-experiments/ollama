fecfceae33f354ae3f2db5ea330ef5ce80589688
Andre Merzky <andre@merzky.net>
2024-11-04 12:53:58 +0100

    snap

--- zmq_baseline/zmq_plot.py
+++ zmq_baseline/zmq_plot.py
@@ -5,7 +5,11 @@ __license__   = 'MIT'
 
 
 import sys
+import time
+import pprint
 
+from   collections       import defaultdict
+from   matplotlib.lines  import Line2D
 import matplotlib.pyplot as plt
 
 import radical.utils     as ru
@@ -28,16 +32,16 @@ metrics = {'request_stop' : {ru.EVENT: 'request_stop' },
 colors  = {'request_stop' : '#CC5555',
            'request_start': '#55CC55'}
 
+c_colors = {'local_noop'  : '#5555CC',
+            'local_llama'  : '#55CC55',
+            'remote_noop' : '#CC5555',
+            'remote_llama' : '#CC55CC'}
+
 
 # ------------------------------------------------------------------------------
 #
-if __name__ == '__main__':
+def plot_rates(src):
 
-    if len(sys.argv) < 2:
-        print("\n\tusage: %s <profile>\n" % sys.argv[0])
-        sys.exit(1)
-
-    src     = sys.argv[1]
     sid     = src[:-5]
     stype   = 'radical'
     session = ra.Session.create(src, stype)
@@ -63,6 +67,110 @@ if __name__ == '__main__':
     plt.ylabel(to_latex('rate (#requests / sec)'))
 
     fig.savefig('%s_rate.png' % sid)
+
+
+# ------------------------------------------------------------------------------
+#
+def plot_scaling():
+
+    idx_c = 0
+    idx_s = 1
+    idx_c = 2
+    idx_t = 3
+
+
+    # data elements: campaign, n_servers, n_clients, n_requests, duration, rate
+
+    data = list()
+    with open('zmq_stats.dat', 'r') as fin:
+        for line in fin.readlines():
+            if '#' in line:
+                continue
+            elems = line.split()
+            data.append( [str(elems[0])]
+                       + [int(val) for val in elems[1:3]]
+                       + [float(elems[5])])
+
+  # pprint.pprint(data)
+
+    # first plots: strong scaling:
+    #   - one plot per noop / llama
+    #   - n_servers = variable
+    #   - x-axis: n_servers
+    #   - y-axis: rate
+    #   - one line per n_clients
+    #   - one color per campaign
+
+    c_names = set([str(x[0]) for x in data])
+
+    for plot_filter in ['noop', 'llama', 'local', 'remote']:
+
+        bundles = defaultdict(dict)
+        fig, ax = plt.subplots(figsize=ra.get_plotsize(300))
+        patches = list()
+        labels  = list()
+
+      # print(plot_filter)
+        plot_data = defaultdict(dict)
+        for c_name in [n for n in c_names if plot_filter in n]:
+
+          # print(c_name)
+            plot_data[c_name] = [x for x in data if c_name == x[0]]
+
+            server_counts = list(set([x[idx_s] for x in plot_data[c_name]]))
+          # print('===', server_counts)
+
+            cnt = 0
+            for sc in sorted(server_counts):
+
+                cnt += 1
+
+                print('------------------------', c_name, sc)
+              # pprint.pprint(plot_data[c_name])
+
+                bundles[c_name][sc] = list()
+                for x in plot_data[c_name]:
+                    if sc == x[idx_s]:
+                        bundles[c_name][sc].append([x[idx_c], x[idx_t]])
+
+              # pprint.pprint(bundles[c_name][sc])
+
+                ax.plot([x[0] for x in bundles[c_name][sc]],
+                        [x[1] for x in bundles[c_name][sc]],
+                        '-o',
+                        color=c_colors[c_name])
+                      # label='%s (%d servers)' % (c_name, sc))
+                # annotate with n_clients
+                last = bundles[c_name][sc][-1]
+                if c_name not in ['remote_noop', 'local_noop']:
+                    ax.annotate('   %4d' % sc, (last[0], last[1]), fontsize=10,
+                                xytext=(10, 0), textcoords='offset points',)
+
+            patch = Line2D([0], [0], color=c_colors[c_name],
+                           linewidth=3, linestyle='-')
+            patches.append(patch)
+            labels.append(c_name)
+
+      # pprint.pprint(bundles)
+
+        ax.set_title('strong scaling (%s)' % plot_filter, loc='center')
+        ax.legend(patches, labels)
+        ax.set_yscale('log')
+        plt.xlabel('n_clients')
+        plt.ylabel('rate (\\#req/s)')
+        fig.savefig('strong_scaling_%s.png' % plot_filter)
+
+
+# ------------------------------------------------------------------------------
+#
+if __name__ == '__main__':
+
+    if len(sys.argv) > 1:
+        for src in sys.argv[1:]:
+            print(src)
+            plot_rates(src)
+
+    plot_scaling()
 
 
 # ------------------------------------------------------------------------------
--- zmq_baseline/zmq_stats.py
+++ zmq_baseline/zmq_stats.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import glob
 import os
+import sys
+import glob
 import radical.utils as ru
 
-dirs = ['local_seeded', 'local_zero', 'remote_seeded', 'remote_zero']
+dirs = ['local_zero', 'local_seed', 'remote_zero', 'remote_seed']
 
+sys.exit()
 
 with open('zmq_stats.dat', 'w') as fout:
 
@@ -15,7 +17,7 @@ with open('zmq_stats.dat', 'w') as fout:
     print(tmp)
 
     for d in dirs:
-        for f in glob.glob('%s/*.prof' % d):
+        for f in sorted(list(glob.glob('%s/*.prof' % d))):
 
             prof   = ru.read_profiles([f])
             sid    = list(prof.keys())[0]
@@ -26,17 +28,16 @@ with open('zmq_stats.dat', 'w') as fout:
             n_services = int(elems[1][1:])
             n_clients  = int(elems[2][1:])
 
+          # print(f)
+          # print(sid, elems, campaign, n_services, n_clients)
+
             starts = [e[0] for e in events if e[1] == 'request_start']
             stops  = [e[0] for e in events if e[1] == 'request_stop']
 
             t_min  = min(starts)
             t_max  = max(stops)
-
             t_diff = t_max - t_min
-
             rate   = len(starts) / t_diff
-
-            sid = os.path.basename(f)[:-5]
 
             tmp = '%20s %4d %4d %12d %12.2f %12.2f' \
                 % (campaign, n_services, n_clients, len(starts), t_diff, rate)
